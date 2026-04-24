c5444d79ea0efcdc474d8f0fb4dffbc7689ed210
Ahmed Madyan <your-email@example.com>
2026-04-24 09:08:23 +0200

    Resolve App.jsx conflict (auth + events)

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,31 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ProtectedRoute from "./components/ProtectedRoute";
-
-function CreateEvent() {
-  return <h1>Create Event Page</h1>;
-}
+import EventsList from "./pages/EventsList";
+import EventDetails from "./pages/EventDetails";
+import CreateEvent from "./pages/CreateEvent";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/sign-in" replace />} />
+      <Route path="/" element={<Navigate to="/events" replace />} />
+
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/sign-in" element={<SignIn />} />
+
+      <Route path="/events" element={<EventsList />} />
+      <Route path="/events/:id" element={<EventDetails />} />
+
       <Route
-        path="/create-event"
+        path="/events/create"
         element={
           <ProtectedRoute>
             <CreateEvent />
           </ProtectedRoute>
         }
       />
+
+      <Route path="*" element={<Navigate to="/events" replace />} />
     </Routes>
   );
 }
